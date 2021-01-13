4b32968ae243c76a0c837e620ff2c08b63f55965
garyndo <garyn.sadewo@gmail.com>
2021-01-13 18:53:40 +0700

    CRUD on CartPage Done

--- kingshoe/src/pages/cartPage.js
+++ kingshoe/src/pages/cartPage.js
@@ -34,10 +34,50 @@ class Cartpage extends React.Component {
             .catch((err) => console.log(err))
 
     }
-    handleDone = () => {
-        console.log('handleDone berhasil')
+    handleDone = (index) => {
+        //1.MENGGANTI DATA PESANAN SUATU PRODUK BERDASARKAN INDEX
+        //cart ada yg d ganti, manipulasi cart,
+        //ambil data yg mau kita edit dr REDUX
+        //tempproduct tempat penyimpnan semntara data product yg ingin d edit 
+        let tempProduct = this.props.cart[index]
+        //kita ganti quantity dan product, mengganti data cart/pesanan untuk product yg ingin d ganti  
+        tempProduct.qty = this.state.newQty //mengganti isi quantity dengan yg baru 
+        tempProduct.total = this.state.newQty * this.props.cart[index].price //update atau mengganti totalprice
+        console.log(tempProduct)
+
+        //2. MEMASUKAN OBJEK PSEANAN YG BARU(TEMP PRODUCT) K DALAM ARRAY CART
+        //tempcart adalah tmpat penampungan smntara data cart yg lama 
+        let tempCart = this.props.cart //panggil array/cart yg lama 
+        tempCart.splice(index, 1, tempProduct) // kita ganti datanya tempcart yg lama dengan tempproduct berisi cart yg baru setelah dedit 
+        //NOTE splice bisa untuk mengganti dengan 3 parameter dan bs jg untk menghapus dengan 2 parameter 
+        console.log(tempCart)
+
+        //3. setelah manipulasi data cart. kita PUSH K DATABASE
+        //KIRIM PERUBAHAN K DATABASE JSON REDUX NIH BOY
+        // GANTI K DATABASE PAKE AXIOS CATCH, UPDATE PAKE AXIOS GET
+        Axios.patch(`http://localhost:2000/users/${this.props.id}`, { cart: tempCart })
+            .then((res) => {
+                console.log(res.data)
+                //update data d redux 
+                Axios.get(`http://localhost:2000/users/${this.props.id}`)
+                    .then((res) => {
+                        this.props.login(res.data)
+                        this.setState({ selectedIndex: null }) // mengubah row tabel seperti semula 
+                    })
+                    .catch((err) => console.log(err))
+            })
+            .catch((err) => console.log(err))
     }
 
+    handleMinus = () => {
+        if (this.state.newQty <= 0) return
+
+        this.setState({ newQty: this.state.newQty - 1 })
+
+    }
+    changeQty = (e) => {
+        this.setState({ newQty: e.target.value })
+    }
 
     renderTHead = () => {
         return (
@@ -68,15 +108,15 @@ class Cartpage extends React.Component {
                                     <Image style={{ width: 100, height: 100 }} src={item.image} rounded />
                                 </td>
                                 <td>{item.size}</td>
-                                <td>{item.price}</td>
+                                <td>IDR {item.price.toLocaleString()}</td>
                                 <td style={{ display: 'flex' }}>
-                                    <Button>-</Button>
-                                    <Form.Control style={{width:'50px'}} placeholder={this.state.newQty} />
-                                    <Button>+</Button>
+                                    <Button onClick={this.handleMinus}>-</Button>
+                                    <Form.Control style={{ width: '100px' }} onChange={(e) => this.changeQty(e)} value={this.state.newQty} />
+                                    <Button onClick={() => this.setState({ newQty: parseInt(this.state.newQty) + 1 })}>+</Button>
                                 </td>
-                                <td>{item.total}</td>
+                                <td>IDR {(this.state.newQty * item.price).toLocaleString()}</td>
                                 <td>
-                                    <Button onClick={this.handleDone} variant='success'>Done</Button>
+                                    <Button onClick={() => this.handleDone(index)} variant='success'>Done</Button>
                                     <Button onClick={() => this.setState({ selectedIndex: null })} variant='info'>Cancel</Button>
                                 </td>
                             </tr>
@@ -90,9 +130,9 @@ class Cartpage extends React.Component {
                                 <Image style={{ width: 100, height: 100 }} src={item.image} rounded />
                             </td>
                             <td>{item.size}</td>
-                            <td>{item.price}</td>
+                            <td>IDR {item.price.toLocaleString()}</td>
                             <td>{item.qty}</td>
-                            <td>{item.total}</td>
+                            <td>IDR {item.total.toLocaleString()}</td>
                             <td>
                                 <Button onClick={() => this.setState({ selectedIndex: index, newQty: item.qty })} variant='warning'>Edit</Button>
                                 <Button onClick={() => this.handleDelete(index)} variant='danger'>Delete</Button>
@@ -104,8 +144,8 @@ class Cartpage extends React.Component {
         )
     }
     render() {
-        console.log(this.props.cart) //caramanggil redux,dan kita liat dapet ga datanya, make sure datanya terpanggil
-        console.log(this.state.selectedIndex)
+        // console.log(this.props.cart) //caramanggil redux,dan kita liat dapet ga datanya, make sure datanya terpanggil
+        // console.log(this.state.selectedIndex)
         return (
             <div style={{ padding: '0 20px' }}>
                 <h1>Welcome to Your Cart Page </h1>
